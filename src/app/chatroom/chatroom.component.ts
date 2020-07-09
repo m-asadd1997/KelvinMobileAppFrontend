8394507fe698c3e140e4c7508d2d8e15b4891638
ibushah <username.ibu@gmail.com>
2020-07-09 22:17:36 +0500

    chatroom search fixes done

--- src/app/chatroom/chatroom.component.ts
+++ src/app/chatroom/chatroom.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChatService } from '../Services/chat.service';
 import { Chatroom } from './Chatroom'
+import { MainService } from '../Services/main.service';
 @Component({
   selector: 'app-chatroom',
   templateUrl: './chatroom.component.html',
@@ -12,20 +13,58 @@ export class ChatroomComponent implements OnInit {
 
   userId = sessionStorage.getItem("userId");
   chatrooms: Array<any> = [];
+  searchName;
+  listOfUsers = [];
+  showError = false;
+  focused = false;
+  screenHeight = null;
+  screenWidth = null;
 
-  constructor(private router: Router, private service: ChatService) {
-
+  constructor(private router: Router, private service: ChatService, private mainService: MainService) {
+    this.onResize();
   }
 
   ngOnInit(): void {
     this.getAllChatrooms();
   }
+  onResize(event?) {
+    this.screenHeight = window.innerHeight - 102;
+    this.screenWidth = window.innerWidth;
 
+    console.log(this.screenHeight)
+  }
 
   goBack() {
     this.router.navigate(['newsfeed']);
   }
+  getUsersOnChange() {
+    this.showError = false;
+    this.checkSearchName()
+    this.listOfUsers = [];
+    this.mainService.searchUsers(this.searchName).subscribe(d => {
 
+      if (d.status == 200) {
+
+        d.result.map(n => {
+          let index = this.listOfUsers.findIndex(d => d.id == n.id);
+          if (!this.listOfUsers.includes(index))
+            this.listOfUsers.push(n);
+        })
+
+      }
+      else {
+        this.showError = true;
+      }
+
+
+    })
+  }
+
+  checkSearchName() {
+    if (this.searchName === "") {
+      this.showError = false;
+    }
+  }
 
   getAllChatrooms() {
     this.service.getAllChatrooms(this.userId)
@@ -34,6 +73,13 @@ export class ChatroomComponent implements OnInit {
 
   chatWith(chatroomId, friendId) {
     this.router.navigate([`chat/${chatroomId}/${friendId}`])
+  }
+
+  initiateChat(friendId) {
+    this.service.initiateChat(this.userId, friendId)
+      .subscribe((chatroom) => {
+        this.router.navigate([`chat/${chatroom}/${friendId}`])
+      })
   }
 
 }
